100a120f5a1a35a5e702cf7bca93fc40167fecc7
Haritha Shree G <harithashree3047@gmail.com>
2026-09-07 14:57:48 +0530

    fix: correct logo import case in Navbar (logo.png -> Logo.png)

--- frontend/src/components/common/Navbar.jsx
+++ frontend/src/components/common/Navbar.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { Link, useLocation } from 'react-router-dom';
-import logoImg from '../../assets/logo.png';
+import logoImg from '../../assets/Logo.png';
 import { Menu, X } from 'lucide-react';
 
 const Navbar = () => {
